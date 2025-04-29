af865caa22d12ac7df41b83fa79b02a6bfbb8b63
João Layon <104402531+DuJao22@users.noreply.github.com>
2025-04-28 21:34:35 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -276,4 +276,4 @@ def upload_photo():
 ###################################
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host="0.0.0.0", port=5000)
+    app.run(app, debug=True, host="0.0.0.0", port=5000)
